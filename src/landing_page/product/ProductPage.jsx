89266f0245e871579b33bf2a6ee3ab52a83de898
PG22 <142742753+PREMGHUNDIYAL@users.noreply.github.com>
2025-04-01 23:25:48 +0530

    Update ProductPage.jsx

--- src/landing_page/product/ProductPage.jsx
+++ src/landing_page/product/ProductPage.jsx
@@ -12,35 +12,44 @@ function PricingPage() {
         imageURL="/media/kite.png" // Ensure correct image path
         productName="Kite"
         productDescription="Our ultra-fast flagship trading platform with streaming market data, advanced charts, an elegant UI, and more. Enjoy the Kite experience seamlessly on your Android and iOS devices."
-        
         tryDemo="#" // Avoid empty href
         learnMore="#"
         googlePlay="/media/googleplay.svg" // Ensure correct image path
         appStore="/media/appstore.svg" // Ensure correct image path
       />
-      <RightSection />
+      <RightSection
+        productName="Console"
+        productDescription="The central dashboard for your Zerodha account. Gain insights into your trades and investments with in-depth reports and visualisations."
+        imageURL="media/console.png"
+        tryDemo="#"
+        learnMore="#"
+      />
+
       <LeftSection
         imageURL="/media/coin.png" // Ensure correct image path
         productName="Coin"
         productDescription="Buy direct mutual funds online, commission-free, delivered directly to your Demat account. Enjoy the investment experience on your Android and iOS devices.."
-        
         tryDemo="#" // Avoid empty href
         learnMore="#"
         googlePlay="/media/googleplay.svg" // Ensure correct image path
         appStore="/media/appstore.svg" // Ensure correct image path
       />
-      <RightSection />
+      <RightSection
+        productName="Kite Connect API"
+        productDescription="Build powerful trading platforms and experiences with our super simple HTTP/JSON APIs. If you are a startup, build your investment app and showcase it to our clientbase."
+        imageURL="media/kiteconnect.png"
+        tryDemo="#"
+        learnMore="#"
+      />
       <LeftSection
         imageURL="/media/varsity.png" // Ensure correct image path
         productName="Varsity mobile"
         productDescription="An easy to grasp, collection of stock market lessons with in-depth coverage and illustrations. Content is broken down into bite-size cards to help you learn on the go.."
-        
         tryDemo="#" // Avoid empty href
         learnMore="#"
         googlePlay="/media/googleplay.svg" // Ensure correct image path
         appStore="/media/appstore.svg" // Ensure correct image path
       />
-
 
       <Universe />
     </>
