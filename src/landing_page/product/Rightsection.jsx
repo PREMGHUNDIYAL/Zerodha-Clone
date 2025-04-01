8e74c29dd0dcbe35eece478bed3a032b3da154ed
PG22 <142742753+PREMGHUNDIYAL@users.noreply.github.com>
2025-04-01 23:25:27 +0530

    Update Rightsection.jsx

--- src/landing_page/product/Rightsection.jsx
+++ src/landing_page/product/Rightsection.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
 
-function RightSection () {
-    return ( <h1>Right Section</h1> );
+function RightSection({ imageURL, productName, productDescription, tryDemo, learnMore }) {
+    return ( 
+        <div className='container mt-5'>
+            <div className='row align-items-center'>
+                {/* Text on the left */}
+                <div className='col-md-6'>
+                    <h1>{productName}</h1>
+                    <p>{productDescription}</p>
+                    <div className="d-flex flex-column">
+                        <a href={tryDemo} className="btn btn-primary mb-2">Try Demo</a>
+                        <a href={learnMore} className="btn btn-outline-secondary mb-2">Learn More</a>
+                    </div>
+                </div>
+
+                {/* Image on the right */}
+                <div className='col-md-6 text-center'>
+                    <img src={imageURL} alt={productName} className="img-fluid" />
+                </div>
+            </div>
+        </div> 
+    );
 }
 
-export default RightSection ;
+export default RightSection;
