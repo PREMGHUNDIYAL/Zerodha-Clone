a97a45a52db338fc20a77791d0f96f36df04894c
PG22 <142742753+PREMGHUNDIYAL@users.noreply.github.com>
2025-04-01 19:36:47 +0530

    Update Team.jsx

--- src/landing_page/about/Team.jsx
+++ src/landing_page/about/Team.jsx
@@ -3,23 +3,19 @@ import React from 'react';
 function Team() {
     return ( 
         <div className ="container">
-             <div className='row p-5   mt-5 mb-5'>
-                <h1 className=' text-center border-top mt-5'>People</h1>
+             <div className='row p-5  border-top  mt-5 '>
+                <h1 className=' text-center  mt-5'>People</h1>
             </div>
-        <div className='row p-5 mt-5 border-top text-muted fs-6' style={{lineHeight:"1.8",fontSize:"1.2em"}}>
-            <div className='col-6 p-5'>
-               <img src="media/ceo.jpg" alt="" />
+        <div className='row p-5  text-muted fs-6' style={{lineHeight:"1.8",fontSize:"1.2em"}}>
+            <div className='col-6 p-5 text-center'>
+               <img src="media/ceo.jpg" alt=""  style={{borderRadius:"100%",width:"60%"}}/>
+               <h4 className='mt-5'>Nithin Kamath</h4>
+               <h6>Founder, CEO</h6>
             </div>
             <div className='col-6 p-5'>
-                <p>
-                In addition, we run a number of popular open online educational and community initiatives to empower retail traders and investors.
-                </p>
-                <p>
-                <a href="" style={{textDecoration:"none"}}>Rainmatter</a>, our fintech fund and incubator, has invested in several fintech startups with the goal of growing the Indian capital markets.
-                </p>
-                <p>
-                And yet, we are always up to something new every day. Catch up on the latest updates on our blog or see what the media is saying about us. 
-                </p>
+                <p>Nithin bootstrapped and founded Zerodha in 2010 to overcome the hurdles he faced during his decade long stint as a trader. Today, Zerodha has changed the landscape of the Indian broking industry.</p>
+                <p >He is a member of the SEBI Secondary Market Advisory Committee (SMAC) and the Market Data Advisory Committee (MDAC).</p>
+                <p >Playing basketball is his zen.</p>
             </div>
         </div>
     </div>
